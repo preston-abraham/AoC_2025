9397021fade1aa29d099047446b9681dfc89aa44
Preston Abraham <112017483+preston-abraham@users.noreply.github.com>
2025-12-02 08:35:58 -0600

    Refactor

--- day2.py
+++ day2.py
@@ -1,27 +1,37 @@
-# Part 1
-invalid = []
-for i in ids:
-    start = int(i.split('-')[0])
-    end = int(i.split('-')[1])
-    for j in range(start,end+1):
-        if not len(str(j)) % 2:
-            midpoint = int(len(str(j)) / 2)
-            if str(j)[:midpoint] == str(j)[midpoint:]:
-                invalid += [str(j)]
-                
-print(sum([int(e) for e in invalid]))
+ids = input.split(',')
 
-# Part 2
-invalid = []
-for i in ids:
-    start = int(i.split('-')[0])
-    end = int(i.split('-')[1])
-    for j in range(start,end+1):
-        num = str(j)
-        for z in range(1,9):
-            if len(num) > z:
+check_map = {
+    1:[],
+    2:[1],
+    3:[1],
+    4:[1,2],
+    5:[1],
+    6:[1,2,3],
+    7:[1],
+    8:[1,2,4],
+    9:[1,3],
+    10:[1,2,5]
+}
+
+def day2(part2 = False):
+    invalid = []
+    for i in ids:
+        start = int(i.split('-')[0])
+        end = int(i.split('-')[1])
+        for j in range(start,end+1):
+            num = str(j)
+            if part2:
+                check_nums = check_map[len(num)]
+            else:
+                if len(num) % 2:
+                    check_nums = []
+                else:
+                    check_nums = [int(len(num) / 2)]
+            for z in check_nums:
                 seq = num[:z]
                 if num.replace(seq,'') == '':
                     invalid += [num]
                     break
-print(sum([int(e) for e in invalid]))
+    return(sum(map(int,invalid)))
+
+print(f'Part 1: {day2(False)} Part 2: {day2(True)}')
